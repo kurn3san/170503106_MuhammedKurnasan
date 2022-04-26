f99bd9bfcb640e3e6af0af7722b989e1443c78d4
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2022-04-26 23:15:44 +0300

    edit for table users

--- demo/src/main/java/com/example/demo/database/configs.java
+++ demo/src/main/java/com/example/demo/database/configs.java
@@ -5,5 +5,5 @@ public class configs {
     protected static final String dbPort = "3306";
     protected static final String dbUser = "root";
     protected static final String dbPass = "admin";
-    protected static final String dbName = "my_database";
+    protected static final String dbName = "db";
 }
