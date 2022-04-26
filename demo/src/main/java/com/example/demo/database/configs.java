8449a0bb833a51e385055c29f36907d8a06a90bd
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2022-04-26 22:53:38 +0300

    db config class

---
+++ demo/src/main/java/com/example/demo/database/configs.java
@@ -0,0 +1,9 @@
+package com.example.demo.database;
+
+public class configs {
+    protected static final String dbHost =  "localhost";
+    protected static final String dbPort = "3306";
+    protected static final String dbUser = "root";
+    protected static final String dbPass = "admin";
+    protected static final String dbName = "my_database";
+}
