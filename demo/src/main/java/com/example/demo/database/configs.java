ecc4c0e03a7741096f1604aba8b8347803496dd1
KURn3SAN <49362763+kurn3san@users.noreply.github.com>
2022-05-01 12:34:56 +0300

    remove exception

--- demo/src/main/java/com/example/demo/database/configs.java
+++ demo/src/main/java/com/example/demo/database/configs.java
@@ -4,6 +4,7 @@ public class configs {
     protected static final String dbHost =  "localhost";
     protected static final String dbPort = "3306";
     protected static final String dbUser = "root";
+
     protected static final String dbPass = "123456789";
     protected static final String dbName = "db";
 }
